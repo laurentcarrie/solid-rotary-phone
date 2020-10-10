e60f2ced8fc9f0ee9766a4b5d53f3a1af77d9256
Laurent Carrié <laurent.carrie@gmail.com>
2020-10-10 12:22:04 +0200

    initial

---
+++ test/test_f1.py
@@ -0,0 +1,14 @@
+from srp import aaa
+
+def func(x):
+    return x + 1
+
+
+class Test1:
+
+    def test_answer(self):
+        assert func(3) == 4
+
+    def test_2(selfs):
+        y = aaa.f()
+        assert y == 3
