509bd2bc8c63b23e6117f758b418da3790f40fce
laurent on mac <laurent.carrie@gmail.com>
2022-02-20 14:03:02 +0100

    ok

--- cxx/to_html.cpp
+++ cxx/to_html.cpp
@@ -10,7 +10,7 @@
 #include <regex>
 #include <array>
 
-std::string exec(const char* cmd) {
+std::string exec(const char *cmd) {
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
@@ -24,94 +24,95 @@ std::string exec(const char* cmd) {
 }
 
 std::string opening_tag(std::string tag) {
-    return std::string("<")+tag+(">") ;
+    return std::string("<") + tag + (">");
 }
 
 std::string closing_tag(std::string tag) {
-    return std::string("</")+tag+(">") ;
+    return std::string("</") + tag + (">");
 }
 
-std::vector<std::string> split_string(std::string input,std::string delim) {
-    std::vector<std::string> ret ;
-    int begin_index = 0 ;
-    while(true) {
-        auto found_index = input.find_first_of(delim, begin_index) ;
-        if ( found_index == std::string::npos ) {
-            ret.push_back(input.substr(begin_index,input.size()-begin_index)) ;
-            break ;
+std::vector<std::string> split_string(std::string input, std::string delim) {
+    std::vector<std::string> ret;
+    int begin_index = 0;
+    while (true) {
+        auto found_index = input.find_first_of(delim, begin_index);
+        if (found_index == std::string::npos) {
+            ret.push_back(input.substr(begin_index, input.size() - begin_index));
+            break;
         }
-        ret.push_back(input.substr(begin_index,found_index-begin_index)) ;
-        begin_index = found_index+1 ;
+        ret.push_back(input.substr(begin_index, found_index - begin_index));
+        begin_index = found_index + 1;
     }
-    return ret ;
+    return ret;
 
 }
 
-std::string get_string_between_tags(std::string input,Item item) {
-    std::string gdata = input.substr(item.starting_+2+item.tag_.size(),item.ending_-item.starting_-2-item.tag_.size()) ;
-    return gdata ;
+std::string get_string_between_tags(std::string input, Item item) {
+    std::string gdata = input.substr(item.starting_ + 2 + item.tag_.size(),
+                                     item.ending_ - item.starting_ - 2 - item.tag_.size());
+    return gdata;
 }
 
-void replace_string_between_tags(std::string& input,std::string replacement,Item item) {
-    input.replace(item.starting_,item.ending_-item.starting_+item.tag_.size()+3,replacement) ;
+void replace_string_between_tags(std::string &input, std::string replacement, Item item) {
+    input.replace(item.starting_, item.ending_ - item.starting_ + item.tag_.size() + 3, replacement);
 }
 
 
-void substitute_V(Item item,std::string& input) {
-    std::string data = get_string_between_tags(input,item) ;
-    std::vector<std::string> lines = split_string(data,"\n") ;
-    std::ostringstream oss ;
-    oss << "<div class=\"verse\">" << std::endl ;
-    for ( auto line : lines ) {
+void substitute_V(Item item, std::string &input) {
+    std::string data = get_string_between_tags(input, item);
+    std::vector<std::string> lines = split_string(data, "\n");
+    std::ostringstream oss;
+    oss << "<div class=\"verse\">" << std::endl;
+    for (auto line: lines) {
         if (!line.empty()) {
             oss << line << " <br>" << std::endl;
         }
     }
-    oss << "</div>" << std::endl ;
-    replace_string_between_tags(input,oss.str(),item) ;
+    oss << "</div>" << std::endl;
+    replace_string_between_tags(input, oss.str(), item);
 }
 
-void substitute_C(Item item,std::string& input) {
-    std::string data = get_string_between_tags(input,item) ;
-    std::vector<std::string> lines = split_string(data,"\n") ;
-    std::ostringstream oss ;
-    oss << "<div class=\"chorus\">" << std::endl ;
-    for ( auto line : lines ) {
+void substitute_C(Item item, std::string &input) {
+    std::string data = get_string_between_tags(input, item);
+    std::vector<std::string> lines = split_string(data, "\n");
+    std::ostringstream oss;
+    oss << "<div class=\"chorus\">" << std::endl;
+    for (auto line: lines) {
         if (!line.empty()) {
             oss << line << " <br>" << std::endl;
         }
     }
-    oss << "</div>" << std::endl ;
-    replace_string_between_tags(input,oss.str(),item) ;
+    oss << "</div>" << std::endl;
+    replace_string_between_tags(input, oss.str(), item);
 }
 
-void substitute_T(Item item,std::string& input) {
-    input.replace(item.ending_, closing_tag(item.tag_).size(), "</div>") ;
-    input.replace(item.starting_, opening_tag(item.tag_).size(), "<div class=\"title\">") ;
+void substitute_T(Item item, std::string &input) {
+    input.replace(item.ending_, closing_tag(item.tag_).size(), "</div>");
+    input.replace(item.starting_, opening_tag(item.tag_).size(), "<div class=\"title\">");
 }
 
-void substitute_L(Item item,std::string& input) {
-    input.replace(item.ending_, closing_tag(item.tag_).size(), "</div>") ;
-    input.replace(item.starting_, opening_tag(item.tag_).size(), "<div class=\"lyrics\">") ;
+void substitute_L(Item item, std::string &input) {
+    input.replace(item.ending_, closing_tag(item.tag_).size(), "</div>");
+    input.replace(item.starting_, opening_tag(item.tag_).size(), "<div class=\"lyrics\">");
 }
 
-void substitute_LY(Config config,Item item,std::string& input) {
-    std::string data = get_string_between_tags(input,item) ;
-    std::cout << "DDDDDDDDDDDDDDDD "   << data << std::endl ;
-    replace_string_between_tags(input,std::string("<div><img class=\"ly\" src=\"")+data+(".png\"></div>"),item) ;
-    std::ostringstream oss ;
-    oss << "lilypond -dbackend=eps -dresolution=600 --png --output " ;
-    oss << config.builddir / config.relpath / data ;
-    std::filesystem::path source (config.srcdir / config.relpath / data ) ;
-    source.replace_extension(".ly") ;
-    oss << " " << source ;
+void substitute_LY(Config config, Item item, std::string &input) {
+    std::string data = get_string_between_tags(input, item);
+    std::cout << "DDDDDDDDDDDDDDDD " << data << std::endl;
+    replace_string_between_tags(input, std::string("<div><img class=\"ly\" src=\"") + data + (".png\"></div>"), item);
+    std::ostringstream oss;
+    oss << "lilypond -dbackend=eps -dresolution=600 --png --output ";
+    oss << config.builddir / config.relpath / data;
+    std::filesystem::path source(config.srcdir / config.relpath / data);
+    source.replace_extension(".ly");
+    oss << " " << source;
     // oss << /home/laurent/work/solid-rotary-phone/build/cranberries/zombies/png_solo /home/laurent/work/solid-rotary-phone/source/cranberries/zombies/png_solo.ly" ;
-    std::cout << oss.str() << std::endl ;
-    exec(oss.str().c_str()) ;
+    std::cout << oss.str() << std::endl;
+    exec(oss.str().c_str());
 
 }
 
-void substitute_LY_WAV(Config config,Item item,std::string& input) {
+void substitute_LY_WAV(Config config, Item item, std::string &input) {
     std::string data = get_string_between_tags(input, item);
     std::stringstream oss;
     auto ly_path = config.srcdir / config.relpath / data;
@@ -132,95 +133,102 @@ void substitute_LY_WAV(Config config,Item item,std::string& input) {
         // make wav
         std::ostringstream oss;
 
-        auto sf2 = "/opt/homebrew/Cellar/fluid-synth/2.2.5/share/fluid-synth/sf2/VintageDreamsWaves-v2.sf2" ;
-        // aufo sf2 = "/usr/share/sounds/sf2/FluidR3_GM.sf2" ;
+        std::vector<std::filesystem::path> soundfont_paths = {
+                "/opt/homebrew/Cellar/fluid-synth/2.2.5/share/fluid-synth/sf2/VintageDreamsWaves-v2.sf2",
+                "/usr/share/sounds/sf2/FluidR3_GM.sf2"};
+        std::vector<std::filesystem::path> existing_soundfont_paths;
+        std::copy_if(soundfont_paths.begin(), soundfont_paths.end(),
+                     std::back_inserter(existing_soundfont_paths), [](std::filesystem::path p) {
+                    return std::filesystem::exists(p);
+                });
 
-        oss << "fluidsynth -F " << wav_path << " " << sf2 << " "  << midi_path;
+        assert(!existing_soundfont_paths.empty());
+
+        oss << "fluidsynth -F " << wav_path << " " << existing_soundfont_paths.front() << " " << midi_path;
         std::cout << oss.str() << std::endl;
         exec(oss.str().c_str());
 
     }
     {
-        std::ostringstream oss ;
-        oss <<  R"here(<div><a class="wav" href=")here" << data << R"here(.wav">)here" << data << "</a></div>";
-        replace_string_between_tags(input,oss.str(),item) ;
+        std::ostringstream oss;
+        oss << R"here(<div><a class="wav" href=")here" << data << R"here(.wav">)here" << data << "</a></div>";
+        replace_string_between_tags(input, oss.str(), item);
     }
 
 
 }
 
 
-
-void substitute_G(Item item,std::string& input) {
-    std::string gdata = get_string_between_tags(input, item) ;
+void substitute_G(Item item, std::string &input) {
+    std::string gdata = get_string_between_tags(input, item);
     // data.replace(item.ending_, closing_tag(item.tag_).size(),"</table></div>") ;
     // data.replace(item.starting_, opening_tag(item.tag_).size(),"<div><table class=\"redtable\">") ;
     // data = std::regex_replace(data,std::regex("#"),"&#x266F;") ;
-    std::vector<std::string> lines = split_string(gdata,"\n") ;
-    std::ostringstream oss ;
-    oss << "<div><table class=\"blueTable\">" << std::endl ;
-    for ( auto line : lines ) {
-        if (line == "") continue ;
-        oss << "<tr>" ;
-        std::vector<std::string> cells = split_string(line,"|") ;
-        for ( auto cell : cells ) {
-            oss << "<td><div style=\"width: 50px\" >" << cell << "</div></td>" ;
+    std::vector<std::string> lines = split_string(gdata, "\n");
+    std::ostringstream oss;
+    oss << "<div><table class=\"blueTable\">" << std::endl;
+    for (auto line: lines) {
+        if (line == "") continue;
+        oss << "<tr>";
+        std::vector<std::string> cells = split_string(line, "|");
+        for (auto cell: cells) {
+            oss << "<td><div style=\"width: 50px\" >" << cell << "</div></td>";
         }
-        oss << "</tr>" << std::endl ;
+        oss << "</tr>" << std::endl;
     }
-    oss << "</table></div>" ;
+    oss << "</table></div>";
 
     // data.replace(item.starting_,item.ending_-item.starting_+std::string("</G>").size(),oss.str()) ;
-    replace_string_between_tags(input, oss.str(), item) ;
+    replace_string_between_tags(input, oss.str(), item);
     // data = std::regex_replace(data,std::regex("#"),"&#x266F;") ;
     // std::cout << input << std::endl ;
 
 }
 
 
-std::string substitute_all_tags(Config config,std::string data) {
+std::string substitute_all_tags(Config config, std::string data) {
 
-    int previous_length=1000 ;
+    int previous_length = 1000;
     while (true) {
-        std::vector<Item> items = parse(data) ;
-        assert (items.size() < previous_length)  ;
-        previous_length = items.size() ;
-        if (items.empty()) break ;
-        Item item = items.front() ;
+        std::vector<Item> items = parse(data);
+        assert (items.size() < previous_length);
+        previous_length = items.size();
+        if (items.empty()) break;
+        Item item = items.front();
 
         // std::cout << "substitute " << item.starting_ << " -> " << item.ending_ << std::endl ;
 
-        Tag etag = tag_of_name((item.tag_)) ;
+        Tag etag = tag_of_name((item.tag_));
         switch (etag) {
             case V:
-                substitute_V(item,data) ;
-                break ;
+                substitute_V(item, data);
+                break;
             case C:
-                substitute_C(item,data) ;
-                break ;
+                substitute_C(item, data);
+                break;
             case T:
-                substitute_T(item,data) ;
-                break ;
+                substitute_T(item, data);
+                break;
             case L:
-                substitute_L(item,data) ;
-                break ;
+                substitute_L(item, data);
+                break;
             case G:
-                substitute_G(item,data) ;
-                break ;
+                substitute_G(item, data);
+                break;
             case LY:
-                substitute_LY(config,item,data) ;
-                break ;
+                substitute_LY(config, item, data);
+                break;
             case LY_WAV:
-                substitute_LY_WAV(config,item,data) ;
-                break ;
+                substitute_LY_WAV(config, item, data);
+                break;
             default:
-                std::ostringstream oss ;
-                oss << __FILE__ << ":" << __LINE__ << " ; case not managed : '" << item.tag_ << "'" ;
-                throw std::runtime_error(oss.str()) ;
+                std::ostringstream oss;
+                oss << __FILE__ << ":" << __LINE__ << " ; case not managed : '" << item.tag_ << "'";
+                throw std::runtime_error(oss.str());
         }
     }
     // data = std::regex_replace(data,std::regex("<br>"),"<br>\n") ;
-    return data ;
+    return data;
 
 
 }
@@ -267,7 +275,7 @@ grid-row-gap: 30px;
                 if (c.irow != irow + 1) continue;
                 if (c.icol != icol + 1) continue;
                 found = true;
-                oss << "<div>" << substitute_all_tags(config,c.data) << "</div>" << std::endl ;
+                oss << "<div>" << substitute_all_tags(config, c.data) << "</div>" << std::endl;
             }
             if (!found) {
                 oss << "<div>" << irow * config.cols.size() + icol << "</div>" << std::endl;
